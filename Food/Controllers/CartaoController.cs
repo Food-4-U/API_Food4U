10cea7c46393d28e168b5e6e677b71d5543630d0
Ozonizador <rnrfnorte@hotmail.com>
2022-06-11 19:31:45 +0100

    Basics of cartao and pedido feitos

---
+++ Food/Controllers/CartaoController.cs
@@ -0,0 +1,22 @@
+﻿using Food.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food.Controllers;
+
+public class CartaoController
+{
+    [HttpGet]
+    [Route("[action]")]
+    public IEnumerable<Cartao> GetCartoesCliente(string id)
+    {
+        return Cartao.GetCartaoCliente(id);
+    }
+    
+
+    [HttpPost]
+    [Route("[action]/{id}")]
+    public string RegistarCartao(Cartao cartao)
+    {
+        return Cartao.AdicionarCartao(cartao);
+    }
+}
